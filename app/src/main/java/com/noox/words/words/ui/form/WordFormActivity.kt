dab464d2aae80a85874de8c8422e73d3d9fbeaf8
Pedro <pedrios@gmail.com>
2018-10-28 01:04:29 +0200

    Add WordFormActivity

---
+++ app/src/main/java/com/noox/words/words/ui/form/WordFormActivity.kt
@@ -0,0 +1,14 @@
+package com.noox.words.words.ui.form
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.noox.words.R
+
+class WordFormActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.word_form_activity)
+  }
+
+}
